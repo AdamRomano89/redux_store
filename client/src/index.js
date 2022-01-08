b343ee67422f3736c8da6d875dc9435069d60039
Adam Romano <adamromano@Adams-iMac.lan>
2022-01-08 12:28:12 -0600

    initializing redux store

--- client/src/index.js
+++ client/src/index.js
@@ -10,9 +10,9 @@ import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
